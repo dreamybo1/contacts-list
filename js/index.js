64c352a33aa5cc8bece00e6f1150f121c5c63366
Александр <dreamy@DreamyMac.local>
2025-03-18 00:01:24 +0500

    changes

--- js/index.js
+++ js/index.js
@@ -434,8 +434,8 @@ function createUserItem (user, firstLetter, placeToInsert) {
     const editImg = document.createElement('img')
     const deleteImg = document.createElement('img')
 
-    editImg.src = "./images/edit.svg"
-    deleteImg.src = "./images/delete.svg"
+    editImg.src = "images/edit.svg"
+    deleteImg.src = "images/delete.svg"
 
     editBtn.classList.add('edit-btn')
     deleteBtn.classList.add('delete-btn')
